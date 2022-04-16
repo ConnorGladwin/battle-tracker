97a10efaec9f5004127ac0eb3046648b7d0b9389
ConnorGladwin <Cygladwin795@gmail.com>
2022-04-16 19:52:26 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import List  from './components/List'
+import './App.css'
+
+function funTitle() {
+  const title = 'Battle!';
+  const funTitle = Array.from(title);
+  return funTitle.map(i => <p className='funTitle'>{i}</p>);
+}
+
+function App() {
+  return (
+    <div className='container'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+     <div className='title'>{funTitle()}</div>
+     <List />
+   </div>
+  )
+}
+
+export default App
